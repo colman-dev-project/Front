f5ca3f0cf9af5525787e050c11628d761b4a14c8
OrtalNisim <ortalnisim012@gmail.com>
2025-04-26 20:28:46 +0300

    added rest parameter and removed the testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import TestImgComponent from './components/shared/Image/TestImgComponent';  // Import the test component
 
 function App() {
   return (
@@ -10,8 +9,6 @@ function App() {
       <Navbar />
       <main style={{ minHeight: '80vh', padding: '2rem' }}>
         <Outlet />
-        <TestImgComponent />
-
       </main>
       <Footer />
     </>
--- src/components/shared/Image/SharedImage.jsx
+++ src/components/shared/Image/SharedImage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import StyledImg from './Image.styles';
+import { StyledImg } from './Image.styles';
 
-const SharedImage = ({ src, alt, style={} }) => {
-  return <StyledImg src={src} alt={alt} sx={style} />;
+const SharedImage = ({ src, alt, style = {}, ...rest }) => {
+  return <StyledImg src={src} alt={alt} sx={style} {...rest} />;
 };
 
 export default SharedImage;
