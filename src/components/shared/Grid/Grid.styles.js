4d06183b67be4617352a330e0d9e2bc11d8b531b
Tal Sadan <talsadan0112@gmail.com>
2025-05-02 15:33:29 +0300

    changed the component accroding to code review

--- src/components/shared/Grid/Grid.styles.js
+++ src/components/shared/Grid/Grid.styles.js
@@ -3,16 +3,23 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 
-export const StyledBox = styled(Box)({
+export const defaultColumns = { xs: 4, sm: 8, md: 12 };
+export const defaultSpacing = { xs: 2, md: 3 };
+export const defaultItemSize = { xs: 2, sm: 4, md: 4 };
+export const defaultStyle = {};
+
+export const StyledBox = styled(Box)(({ theme }) => ({
   width: '100%',
-});
+  flexGrow: 1,
+}));
+
 
 export const StyledGrid = styled(Grid)(({ theme }) => ({
   display: 'flex',
 }));
 
 export const StyledItem = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#fff',
+  backgroundColor: theme.palette.background.paper,
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: 'center',
--- src/components/shared/Grid/SharedGrid.jsx
+++ src/components/shared/Grid/SharedGrid.jsx
@@ -1,25 +1,33 @@
 import * as React from 'react';
-import { StyledBox, StyledGrid, StyledItem } from './Grid.styles';
+import {
+  defaultColumns,
+  defaultSpacing,
+  defaultItemSize,
+  defaultStyle,
+  StyledBox,
+  StyledGrid,
+  StyledItem
+} from './Grid.styles';
 
 function SharedGrid({
   items = [],
-  columns = { xs: 4, sm: 8, md: 12 },
-  spacing = { xs: 2, md: 3 },
-  itemSize = { xs: 2, sm: 4, md: 4 },
-  style = {},
+  columns = defaultColumns,
+  spacing = defaultSpacing,
+  itemSize = defaultItemSize,
+  style = defaultStyle,
   ...rest
 }) {
   return (
-    <StyledBox sx={{ flexGrow: 1 }} style={style}>
+    <StyledBox style={style}>
       <StyledGrid
         container
         spacing={spacing}
         columns={columns}
         {...rest}
       >
-        {items.map((item, index) => (
-          <StyledGrid item key={index} {...itemSize}>
-            <StyledItem>{item}</StyledItem>
+        {items.map((item) => (
+          <StyledGrid item key={item.id} {...itemSize}>
+            <StyledItem>{item.description}</StyledItem>
           </StyledGrid>
         ))}
       </StyledGrid>
