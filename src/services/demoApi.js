4ed9f30b1cefc04431fbc8164f97bf9645023e7f
OrtalNisim <ortalnisim012@gmail.com>
2025-05-13 16:29:49 +0300

    changed queryFn to a real api request

--- src/services/demoApi.js
+++ src/services/demoApi.js
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const DEMO_LOADING_DELAY_MS = 800;
 import { ROUTES as ROUTER_PATHS } from '../constants/routerPaths';
 
 export const demoApi = createApi({
@@ -7,13 +6,11 @@ export const demoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: ROUTER_PATHS.HOME }),
   endpoints: (builder) => ({
     AddToCart: builder.mutation({
-      async queryFn() {
-        return new Promise((resolve) =>
-          setTimeout(() => {
-            resolve({ data: {} });
-          }, DEMO_LOADING_DELAY_MS),
-        );
-      },
+      query: (product) => ({
+        url: '/cart',
+        method: 'POST',
+        body: product,
+      }),
     }),
   }),
 });
