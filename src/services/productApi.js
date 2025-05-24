22d3f065cb35a9018301ee7e537a20fb7bfc227e
OrtalNisim <ortalnisim012@gmail.com>
2025-05-24 20:17:55 +0300

    created an addProduct request

--- src/services/productApi.js
+++ src/services/productApi.js
@@ -1,15 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-
 export const productApi = createApi({
   reducerPath: 'productApi',
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_SERVER_URL }),
+  tagTypes: ['Product'],
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => '/products',
       providesTags: ['Product'],
     }),
-
     getNewestProducts: builder.query({
       query: () => '/products?limit=8&sort=newest',
       providesTags: ['Product'],
@@ -18,16 +17,24 @@ export const productApi = createApi({
       query: (productId) => ({
         url: `/products/${productId}`,
         method: 'DELETE',
-        invalidatesTags: ['Product'],
       }),
-      updateProduct: builder.mutation({
-        query: ({ productId, updatedData }) => ({
-          url: `/products/${productId}`,
-          method: 'PATCH',
-          body: updatedData,
-        }),
-        invalidatesTags: ['Product'],
+      invalidatesTags: ['Product'],
+    }),
+    updateProduct: builder.mutation({
+      query: ({ productId, updatedData }) => ({
+        url: `/products/${productId}`,
+        method: 'PATCH',
+        body: updatedData,
       }),
+      invalidatesTags: ['Product'],
+    }),
+    addProduct: builder.mutation({
+      query: (newProduct) => ({
+        url: '/products',
+        method: 'POST',
+        body: newProduct,
+      }),
+      invalidatesTags: ['Product'],
     }),
   }),
 });
@@ -37,4 +44,5 @@ export const {
   useGetNewestProductsQuery,
   useDeleteProductMutation,
   useUpdateProductMutation,
+  useAddProductMutation,
 } = productApi;
