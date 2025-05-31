5aa912a30881aeb387e3cae693d64a3b327ea849
roiyraz88 <roiyraz88@gmail.com>
2025-05-31 14:46:56 +0300

    Fixed the RTK logic according the CR.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axiosInstance from '../../api/axiosInstance'; 
-import LoginForm from '../components/Forms/LoginForm'
+import LoginForm from '../components/Forms/LoginForm';
 import authStorageService from '../../services/authStorageService';
-import { API_PATHS } from '../constants/auth.constants'
+import { useLoginMutation } from '../features/api/apiSlice';
 import SharedTypography from '../components/shared/Text/SharedText';
+import { ROUTES } from '../constants/routerPaths';
+import {
+  AUTH_ERRORS,
+  AUTH_RESPONSE_KEYS,
+} from '../constants/auth.constants'; 
 
 export default function LoginPage() {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
+  const [login, { isLoading }] = useLoginMutation();
+
   const handleLogin = async (data) => {
     try {
-      const response = await axiosInstance.post(API_PATHS.LOGIN, data);
-      const { token } = response.data;
+      const response = await login(data).unwrap();
+      const token = response[AUTH_RESPONSE_KEYS.TOKEN];
+
+      if (!token) {
+        throw new Error(AUTH_ERRORS.INVALID_LOGIN_RESPONSE);
+      }
 
       authStorageService.setToken(token);
-
       setError('');
-      navigate('/'); 
+      navigate(ROUTES.HOME); 
     } catch (err) {
-      setError(err.response?.data?.error || 'Login failed');
+      console.error('Login error:', err);
+      const message =
+        err?.data?.error ||
+        err?.data?.message ||
+        err?.message ||
+        AUTH_ERRORS.GENERAL_LOGIN_ERROR;
+      setError(message);
     }
   };
 
