07dc26c176b77e9c03bc6a451c493cbaa6d2733e
OfekSagiv <ofeksagiv@gmail.com>
2025-06-19 19:37:53 +0300

    fixed issues in login page

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState,useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useLoginUserMutation } from '../../services/authApi.js';
-import { ROUTES } from '../../constants/routerPaths.js';
-import { AUTH_ERRORS } from '../../constants/auth.constants.js';
+
 import LoginForm from '../../components/Forms/LoginForm.jsx';
 import SharedTypography from '../../components/shared/Text/SharedText.jsx';
+import { AUTH_ERRORS } from '../../constants/auth.constants.js';
+import { ROUTES } from '../../constants/routerPaths.js';
+import { useLoginUserMutation } from '../../services/authApi.js';
+import { extractedAuthError } from '../../utils/authErrors.js';
 
 export default function LoginPage() {
   const [error, setError] = useState('');
@@ -14,32 +16,23 @@ export default function LoginPage() {
   const [loginUser, { isLoading }] = useLoginUserMutation();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-  const handleLogin = async (formData) => {
-    if (localStorage.getItem('accessToken')) {
-      return;
-    }
-
-
-    try {
-      await loginUser(formData).unwrap();
-
-      setError('');
-      navigate(ROUTES.HOME);
-    } catch (err) {
-      console.error(AUTH_ERRORS.LOGIN_ERROR, err);
-      const message =
-        err?.data?.error ||
-        err?.data?.message ||
-        err?.message ||
-        AUTH_ERRORS.GENERAL_LOGIN_ERROR;
-      setError(message);
-    }
-  };
   useEffect(() => {
     if (isLoggedIn) {
       navigate(ROUTES.HOME);
     }
   }, [isLoggedIn, navigate]);
+
+  const handleLogin = async (credentials) => {
+    try {
+      await loginUser(credentials).unwrap();
+
+      setError('');
+      navigate(ROUTES.HOME);
+    } catch (err) {
+      console.error(AUTH_ERRORS.LOGIN_ERROR, err);
+      setError(extractedAuthError(err, AUTH_ERRORS.GENERAL_LOGIN_ERROR));
+    }
+  };
 
   return (
     <div>
